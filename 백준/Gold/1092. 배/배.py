af6d2d54557213c99b757554d9b0bfc6080a205e
ykm1256 <64515926+ykm1256@users.noreply.github.com>
2023-03-30 17:19:23 +0900

    [Gold V] Title: 배, Time: 4820 ms, Memory: 115268 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,25 @@
+import sys
+si = sys.stdin.readline
+
+
+N = int(si().rstrip())
+crain = list(map(int, si().split()))
+crain.sort()
+M = int(si().rstrip())
+box = list(map(int,si().split()))
+box.sort()
+
+
+ans = 0
+if box[-1] > crain[-1]:
+    box = []
+    ans = -1
+while box:
+    for i in range(N-1, -1, -1):
+        for j in range(M-1, -1, -1):
+            if box[j] <= crain[i]:
+                box.remove(box[j])
+                M -= 1
+                break
+    ans += 1
+print(ans)
