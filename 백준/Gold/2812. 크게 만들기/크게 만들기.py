bdafe42b5062cd7c3b59d2208745159b9e62f472
ykm1256 <64515926+ykm1256@users.noreply.github.com>
2023-04-01 21:23:02 +0900

    [Gold III] Title: 크게 만들기, Time: 304 ms, Memory: 43364 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -1,22 +1,11 @@
 import sys
 si = sys.stdin.readline
 
-
 N, K = map(int, si().split())
 
 num = list(map(int, si().rstrip()))
 
 ans = []
-
-def search(s, e, m):
-	maxValue = m
-	index = 0
-	for i in range(s, e):
-		if num[i] > maxValue:
-			maxValue = num[i]
-			index = i
-	
-	return index
 
 for i in range(N):
 	if not ans:
