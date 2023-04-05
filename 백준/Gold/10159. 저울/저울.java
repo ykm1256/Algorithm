1eb6d65a679451b367501790691b03ebf471a593
ykm1256 <64515926+ykm1256@users.noreply.github.com>
2023-04-05 09:27:25 +0900

    [Gold IV] Title: 저울, Time: 128 ms, Memory: 12436 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] shorter, taller;
+
+    static int[] chkS, chkT;
+    static int n, m;
+
+    static int dirShort(int node){
+        int ret = 1;
+        for (int i = 0; i < n; i++) {
+            if (chkS[i] == 0 && shorter[node][i] == 1){
+                chkS[i] = 1;
+                ret += dirShort(i);
+            }
+        }
+        return ret;
+    }
+
+    static int dirTall(int node){
+        int ret = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (chkT[i] == 0 && taller[node][i] == 1){
+                chkT[i] = 1;
+                ret += dirTall(i);
+            }
+        }
+        return ret;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        shorter = new int[n][n];
+        taller = new int[n][n];
+        chkS = new int[n];
+        chkT = new int[n];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            shorter[b][a] = 1;
+            taller[a][b] = 1;
+        }
+
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            chkS = new int[n];
+            chkT = new int[n];
+            chkS[i] = 1;
+            chkT[i] = 1;
+            int s = dirShort(i);
+            int t = dirTall(i);
+            System.out.println(n-1 - (s+t-2));
+        }
+    }
+}
