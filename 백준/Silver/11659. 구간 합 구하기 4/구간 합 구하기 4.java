564b547092328919552f35d84720bacf3e35cd64
ykm1256 <64515926+ykm1256@users.noreply.github.com>
2023-04-07 12:23:25 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2032 ms, Memory: 66536 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int numbers[] = new int[n];
-        int sum[] = new int[n];
-        for (int i = 0;i<n;i++){
-            numbers[i] = sc.nextInt();
-            if (i == 0)
-                sum[i] = numbers[i];
-            else{
-                sum[i] = sum[i-1] + numbers[i];
-            }
+    static int[] tree;
+    static int n;
+
+    static int sum(int i) {
+        int ans = 0;
+        while (i > 0) {
+            ans += tree[i];
+            i -= (i & -i);
         }
+        return ans;
+    }
 
-        for (int t=0;t<m;t++){
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            if (s == 1)
-                System.out.println(sum[e-1]);
-            else
-                System.out.println(sum[e-1]-sum[s-2]);
+    static void update(int i, int num) {
+        while (i <= n) {
+            tree[i] += num;
+            i += (i & -i);
         }
+    }
 
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        tree = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            update(i, Integer.parseInt(st.nextToken()));
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            System.out.println(sum(e) - sum(s-1));
+        }
     }
 }
