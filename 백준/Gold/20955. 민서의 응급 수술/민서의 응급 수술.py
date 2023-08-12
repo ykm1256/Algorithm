4fab6fc63f7876dd7553fc961eb0715ae7e46aae
ykm1256 <64515926+ykm1256@users.noreply.github.com>
2023-08-12 22:06:56 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 220 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
@@ -1,37 +1,34 @@
 import sys
+si = sys.stdin.readline
 
-input=sys.stdin.readline
+n, m = map(int, si().split())
 
-m,n=map(int,input().split())
+parents = list(range(n+1))
 
-parent=list(range(m+1))
+def find(x):
+	if x != parents[x]:
+		parents[x] = find(parents[x])
+	return parents[x]
 
-def find_parent(parent,x):
-    if parent[x] !=x:
-        return find_parent(parent,parent[x])
-    return x
+def union(a, b):
+	pa = find(a)
+	pb = find(b)
+	if pa == pb:
+		return True
+	if pa < pb:
+		parents[pb] = pa
+	elif pa > pb:
+		parents[pa] = pb
+	return False
 
-def union_parent(parent,a,b):
-    a = find_parent(parent,a)
-    b = find_parent(parent,b)
-    if a<b:
-        parent[b]=a
-    else:
-        parent[a]=b
+cnt = 0
+for _ in range(m):
+	u, v = map(int, si().split())
+	if union(u, v):
+		cnt += 1
 
-cnt=0
+for i in range(1, n+1):
+	if find(i) == i:
+		cnt += 1
 
-for i in range(n):
-    st,ed=map(int,input().split())
-    # union 하기 전인데 부모가 같다면 싸이클이 존재한다는 것
-    if find_parent(parent,st)==find_parent(parent,ed):
-        cnt+=1
-    union_parent(parent,st,ed)
-    
-link=0
-for i in range(1,m):
-    if find_parent(parent,i)!=find_parent(parent,i+1):
-        union_parent(parent,i,i+1)
-        link+=1
-
-print(cnt+link)
+print(cnt - 1)
